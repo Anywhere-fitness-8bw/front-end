1056ec9ebe52ef7e5d98b3e66fdc2e61e5f9f520
Visnu <vgcaisse@gmail.com>
2021-12-20 18:07:43 -0500

    HOLY I FORGOT ABOUT COMMITING!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import { Link, Route, Switch } from 'react-router-dom';
+// import axios from 'axios';
+// import schema from './validation/FormSchema';
+// import * as yup from 'yup';
+
+// import Home from './components/Home';
+import ItemsList from './components/ItemsList'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ItemsList />
     </div>
   );
 }
