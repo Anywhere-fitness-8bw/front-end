808327fb90b824a3fc2ea3a32e11f9d58e2de919
Visnu <vgcaisse@gmail.com>
2021-12-21 17:23:27 -0500

    finished the styling for the page, will now start on the functionality

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,105 +1,86 @@
 import React from 'react';
-import styled from "styled-components";
+import { StyledH1, 
+        StyledH2, 
+        StyledH3, 
+        StyledContainer, 
+        StyledClassOptions,
+        StyledContainerL,
+        StyledContainerR,
+        StyledButton,
+        StyledImgs,
+        StyledClassContainers
+    } from './Styles';
 
-const StyledContainer = styled.div`
-  display:flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const StyledButton = styled.button`
-  min-width: 13%;
-  border: none;
-  font-size: 18px;
-  padding: 7px 10px;
-  color:green;
-  transition: all .2s;
-  margin-top: 5%;
-
-  &&:hover {
-      cursor: pointer;
-      background-color: green;
-      transform: scale(1.125);
-      color: white;
-      transition: ease-in-out .2s;
-  }
-`;
-
-const StyledImgs = styled.img`
-        
-`;
-
-const StyledClassOptions = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    flex-wrap: wrap;
-`;
-
-const StyledClassContainters = styled.label`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    flex-wrap: wrap;
-`;
 
 export default function ItemsList(props) {
 
     return (        
         <StyledContainer>
-            <h1>Select Your Class</h1>
+            <StyledH1>Select Your Class</StyledH1>
+            <StyledH2>Available Classes</StyledH2>
                 
             <form className='classForm'> 
-                <h2>Class Intensity</h2>
-                <label className='intensityDropdown'>Intensity: 
+
+                <StyledClassOptions>                     
+                    <StyledContainerL>           
+                        <StyledClassContainers>
+                            <StyledImgs src='https://www.vmcdn.ca/f/files/via/import/2017/12/31113259_shutterstock_749969473.jpg;w=960' alt='Image of Crossfit'/>
+                            <StyledH3>Crossfit</StyledH3>                        
+                            <input
+                                type='checkbox'
+                            />
+                        </StyledClassContainers>
+
+                        <StyledClassContainers>
+                            <StyledImgs src='https://cdn.shopify.com/s/files/1/0327/8461/3514/articles/powerlifting-exercises-for-beginners-1024x576_1024x.jpg?v=1582312375' alt='Image of PowerLifters'/>
+                            <StyledH3>Power Lifting</StyledH3>
+                            <input
+                                type='checkbox'
+                            />
+                        </StyledClassContainers>
+                    </StyledContainerL>      
+
+                    <StyledContainerR>
+                        <StyledClassContainers>
+                            <StyledImgs src='https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY0OTE4ODAzMDQwMzE0Njg1/yoga-gettyimages-1142820079-promo.jpg' alt='Image of Our Yoga Instructors'/>
+                            <StyledH3>Yoga</StyledH3>
+                            <input
+                                type='checkbox'
+                            />
+                        </StyledClassContainers>
+
+                        <StyledClassContainers>
+                            <StyledImgs src='https://www.verywellfit.com/thmb/WtaRzGOCbJdVYFlWr_7VOfwn_Ow=/3000x2002/filters:no_upscale():max_bytes(150000):strip_icc()/zumba-fatcamera-c9d4ee824a0f4fda883484f878abc8ae.jpg' alt='Image of Our Zumba Meets'/>
+                            <StyledH3>Zumba</StyledH3>
+                            <input
+                                type='checkbox'
+                            />
+                        </StyledClassContainers>
+                    </StyledContainerR>
+                </StyledClassOptions>
+
+                <StyledClassContainers>
+                <StyledH2>Class Intensity</StyledH2>
+                <label className='intensityDropdown'>
                     <select >
                         <option value={""}>-- Select your Intensity --</option>
 
-                        <option value='Small'>Mild</option>
-                        <option value='Medium'>Medium</option>
-                        <option value='Large'>Intense</option>
+                        <option value='mild'>Mild</option>
+                        <option value='medium'>Medium</option>
+                        <option value='intense'>Intense</option>
                     </select>
                 </label>
-
-                
-                <StyledClassOptions>
-                    <h2>Available Classes</h2>                    
-                    <StyledClassContainters>Crossfit:
-                        <img src='' alt='Image of Crossfit'/>
-                        <input
-                            type='checkbox'
-                        />
-                    </StyledClassContainters>
-
-                    <StyledClassContainters>Power Lifting: 
-                        <img src='' alt='Image of PowerLifters'/>
-                        <input
-                            type='checkbox'
-                        />
-                    </StyledClassContainters>
-
-                    <StyledClassContainters>Yoga: 
-                        <img src='' alt='Image of Our Yoga Instructors'/>
-                        <input
-                            type='checkbox'
-                        />
-                    </StyledClassContainters>
-
-                    <StyledClassContainters>Zumba: 
-                        <img src='' alt='Image of Our Zumba Meets'/>
-                        <input
-                            type='checkbox'
-                        />
-                    </StyledClassContainters>
-                </StyledClassOptions>
-
-                <h3>Would You Like to be A Part of Our Newsletter?</h3>
-                <label email='email'>Email: 
+                </StyledClassContainers>
+                                
+                <StyledContainer>
+                <StyledH3>Would You Like to be A Part of Our Newsletter?</StyledH3>
+                <label email='email'>
                     <input
                         type="email"
                         placeholder="Please Input Your Email"
                     />
                 </label>
+                </StyledContainer>
             </form>            
            <StyledButton className='submitbtn' type='submit'>Sign Up for Your Class!</StyledButton> 
         </StyledContainer>
---
+++ src/components/Styles.js
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+
+export const StyledH1 = styled.h1`
+    color: white;
+`;
+
+export const StyledH2 = styled.h2`
+    color: white;
+`;
+
+export const StyledH3 = styled.h3`
+    color: white;
+`;
+
+export const StyledContainer = styled.div`
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  background-color:  #FE6862;
+  margin: 2rem 2rem;
+`;
+
+export const StyledClassOptions = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    text-align: center;
+    align-items: center;
+`;
+
+export const StyledContainerL = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex-wrap: wrap;
+    margin: auto 5%;
+    text-align: center;
+`;
+
+export const StyledContainerR = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    flex-wrap: wrap;
+    margin: auto 5%;
+    text-align: center;
+`;
+
+export const StyledButton = styled.button`
+  min-width: 13%;
+  border: none;
+  font-size: 18px;
+  padding: 7px 10px;
+  color: #FE6862;
+  transition: all .2s;
+  margin-top: 3%;
+  margin-bottom: 3.5%;
+
+  &&:hover {
+      cursor: pointer;
+      background-color: white;
+      transform: scale(1.125);
+      color: #FE6862;
+      transition: ease-in-out .2s;
+      box-shadow: 5px 5px 2px 1px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const StyledImgs = styled.img`
+    transition: all .2s;
+    width: 25rem;
+    height: 20rem;
+    margin-top: 10%;
+
+    &&:hover {
+      cursor: pointer;
+      transform: scale(1.05);
+      transition: ease-in-out .2s;
+      box-shadow: 10px 10px 2px 1px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const StyledClassContainers = styled.label`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    flex-wrap: wrap;
+`;
